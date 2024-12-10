308d62448ad215d52f02511170afe21da7b5408d
Pavlo Rasskazov <Pavlo.Rasskazov@x-insight.pl>
2024-12-10 16:22:36 +0100

    Data Types

---
+++ DataTypes/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int age = 40;
+    double gpa = 3.7;
+    char grade = 'A';
+    char phrase[] = "Giraffe Academy";
+
+    printf("Age: %d\n", age);
+    printf("GPA: %f\n", gpa);
+    printf("Grade: %c\n", grade);
+    printf("Phrase: %s\n", phrase);
+
+    return 0;
+}
